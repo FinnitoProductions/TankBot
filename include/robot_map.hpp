a69b0ce4cf09f0724b4bf5fa8699e79f4906087e
Finn Frankis <finn@teachmy.com>
2019-06-02 11:18:29 -0700

    Add correct pins for left and right drive

--- include/robot_map.hpp
+++ include/robot_map.hpp
@@ -5,10 +5,10 @@
 * @since 6/1/19
 */
 
-const int FORWARD_LEFT_DRIVE = 0;
-const int REVERSE_LEFT_DRIVE = 0;
-const int ENABLE_LEFT_DRIVE = 0;
+const int FORWARD_LEFT_DRIVE = 27;
+const int REVERSE_LEFT_DRIVE = 22;
+const int ENABLE_LEFT_DRIVE = 17;
 
-const int FORWARD_RIGHT_DRIVE = 0;
-const int REVERSE_RIGHT_DRIVE = 0;
-const int ENABLE_RIGHT_DRIVE = 0;
+const int FORWARD_RIGHT_DRIVE = 2;
+const int REVERSE_RIGHT_DRIVE = 3;
+const int ENABLE_RIGHT_DRIVE = 4;
